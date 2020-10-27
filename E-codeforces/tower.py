f3958327c73189b5cd593d59e98f93643c7b08cf
Mohammad Izhan <43714869+izhan9637@users.noreply.github.com>
2020-10-27 12:50:35 +0530

    Add files via upload
    Codeforces Type E Probems.

---
+++ E-codeforces/tower.py
@@ -0,0 +1,19 @@
+# Link: http://codeforces.com/contest/777/problem/E
+n = int(input())
+d = []
+for i in range(n):
+    a, b, h = map(int, input().split())
+    d.append((b, a, h))
+d.sort()
+ht = d[len(d) - 1][2]
+ans = ht
+s = [d[len(d) - 1]]
+
+for i in range(n - 2, -1, -1):
+    while s and d[i][0] <= s[len(s) - 1][1]:
+        ht -= s.pop()[2]
+    s.append(d[i])
+    ht += d[i][2]
+    if ans < ht:
+        ans = ht
+print(ans)
